6ecea85064d384abcf57cf02ac189373ac6b8e7e
Faouzi Elbakri <56311060+Faouzielbakri@users.noreply.github.com>
2021-04-25 19:14:18 +0000

    firebase file configs changed

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDN-qLMZN3SaTK8h3ZP6-rWkYItG0ETKuw",
-  authDomain: "essafacours.firebaseapp.com",
-  projectId: "essafacours",
-  storageBucket: "essafacours.appspot.com",
-  messagingSenderId: "413648904712",
-  appId: "1:413648904712:web:84ec96f7428afcaf27e7bd",
-  measurementId: "G-6XJKMQ54FQ",
+  apiKey: "AIzaSyCkjPw1a4T2m26XZU-kOk7dSdWJwgiT6ek",
+  authDomain: "pro--student.firebaseapp.com",
+  projectId: "pro--student",
+  storageBucket: "pro--student.appspot.com",
+  messagingSenderId: "945775953051",
+  appId: "1:945775953051:web:2a37017d41800ec85e323e",
+  measurementId: "G-F8TH3HZ0JF",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
@@ -14,4 +14,4 @@ const storage = app.storage();
 const db = app.firestore();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 const auth = app.auth();
-export { db, storage, auth,googleProvider };
+export { db, storage, auth, googleProvider };
